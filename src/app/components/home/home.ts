e02d5e1b8778b27322c9eab5482244e5bb9a4e26
appl4e <appl4e@gmail.com>
2025-12-10 21:47:50 +0600

    template driven form example

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'home',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  user = {
+    name: '',
+    email: '',
+    age: '',
+  };
+  onSubmit(formData: any) {
+    console.log(formData);
+  }
+}
