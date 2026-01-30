204e92ce7455b60d480a42e6749c76d2b2625362
appl4e <appl4e@gmail.com>
2026-01-30 16:05:01 +0600

    Add reactive form fields and validation for password confirmation

--- src/app/components/reactive-form/reactive-form.ts
+++ src/app/components/reactive-form/reactive-form.ts
@@ -1,6 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+function matchPassword(c: AbstractControl) {
+  const pass = c.get('password')?.value;
+  const confPass = c.get('confirm_password')?.value;
+
+  return pass === confPass ? null : { passwordMismatch: true };
+}
 
 @Component({
   selector: 'reactive-form',
@@ -11,18 +24,30 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 export class ReactiveForm {
   userFormGroup: FormGroup;
 
+  get field() {
+    return this.userFormGroup.controls;
+  }
+
   constructor(private fb: FormBuilder) {
-    this.userFormGroup = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(5)]],
-      email: ['', [Validators.required, Validators.email]],
-      age: [null, [Validators.required, Validators.min(10)]],
-    });
+    this.userFormGroup = this.fb.group(
+      {
+        name: ['', [Validators.required, Validators.minLength(5)]],
+        email: ['', [Validators.required, Validators.email]],
+        age: [null, [Validators.required, Validators.min(10)]],
+        password: ['', [Validators.required]],
+        confirm_password: ['', [Validators.required]],
+      },
+      {
+        validators: matchPassword,
+      }
+    );
   }
 
   onSubmit() {
     if (this.userFormGroup.valid) {
       console.log(this.userFormGroup.value);
     } else {
+      console.log(this.userFormGroup);
     }
   }
 }
